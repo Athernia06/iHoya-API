77782a98f4a38c4b49d2120f0161f0b552b50d72
Muhammad Rafi <athernia06@gmail.com>
2023-07-15 19:51:53 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -25,9 +26,11 @@ class AuthController extends Controller
     ]);
     try {
       $user = new User;
+      $user->username = $request->username;
+      $user->role = $request->role;
       $user->name = $request->name;
       $user->email = $request->email;
-      $user->password = bcrypt($request->password);
+      $user->password = Hash::make($request->password);
       $user->save();
       return response()->json(["user" => $user, "message" => "CREATED"], 201);
     } catch (\Exception $e) {
